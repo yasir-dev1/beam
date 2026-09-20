857bdcce041d920d712d2ac4d1c646ea32482ca6
yasir-dev1 <yasireposta3@gmail.com>
2026-09-20 10:22:58 +0300

    Init Cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Args,Parser};
+
+
+#[derive(Parser,Debug)]
+#[command(name = "beam" , override_usage = "beam --send <file>\n       beam <code>"
+)]
+#[command(about = "beam - transfer files between computers")]
+#[command(arg_required_else_help(true))]
+struct Cli{
+    #[arg(short,long,value_name="file")]
+    send:Option<String>,
+
+    code:Option<String>,
 }
+
+
+fn main() {
+    let args = Cli::parse();
+
+    if let Some(value) = args.send  {
+        // TODO: SEND
+    }else if let Some(value) = args.code {
+        // TODO: RECIVE
+    }
+}
+
