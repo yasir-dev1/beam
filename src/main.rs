ab2c7b180fde96d2fac04576d962dfeeb9644999
yasir-dev1 <yasireposta3@gmail.com>
2026-09-26 13:34:20 +0300

    feat: initialize TCP listener for data transfer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use clap::{Parser};
-use hostname::get;
 use crate::transfer::{recive, send};
 
 
--- src/transfer.rs
+++ src/transfer.rs
@@ -2,13 +2,41 @@
 use rand::{distr::Alphanumeric,RngExt};
 use mdns_sd::{ResolvedService, ServiceDaemon, ServiceEvent, ServiceInfo};
 use local_ip_address::local_ip;
-use std::{fs::File, sync::{Arc, Mutex}};
+use std::{fs::File, io::{Read, Write}, net::{IpAddr, TcpListener, TcpStream}, sync::{Arc, Mutex}};
 use daemonize::Daemonize;
+
 
 fn gen_code() -> String {
     let mut rng = rand::rng();
     let chars:String = (0..7).map(|_| rng.sample(Alphanumeric) as char).collect();
     return  chars;
+}
+
+fn handle_clinet(mut stream:TcpStream){
+    let mut buffer = [0;1024];
+    stream.read(&mut buffer).expect("Error Reading");
+    let request = String::from_utf8_lossy(&buffer[..]);
+    println!("Request : {request:?}");
+    let response = "Hello\n".as_bytes();
+    stream.write(&response).expect("Error Sending Response");
+}
+
+fn register_service(host_name:&String,current_ip:&IpAddr,code:&String) -> std::result::Result<(),Box<dyn  std::error::Error>> {
+    let properties = [("code", code.as_str())];
+    let daemon = ServiceDaemon::new()?;
+
+    let service = ServiceInfo::new(
+        "_beam._tcp.local.",
+        "Beam",
+        host_name,
+        current_ip,   
+        53317,
+        &properties[..],
+    )?;
+
+    let _ = daemon.register(service)?;
+    
+    Ok(())
 }
 
 pub fn send(path: &str) -> std::result::Result<String, Box<dyn std::error::Error>> {
@@ -27,28 +55,29 @@ pub fn send(path: &str) -> std::result::Result<String, Box<dyn std::error::Error
 
       
         println!("Your code is {}",code);
+         
         match daemonize.start() {
             Ok(_) => { 
                 let host_name = format!("{}.local.", hostname::get()?.to_string_lossy());
-                let properties = [("code", code.as_str())];
                 let current_ip = local_ip()?;
+                let ip_str = current_ip.to_string();
+                let addr = format!("{ip_str}:53317");
                 
-                let daemon = ServiceDaemon::new()?;
-
-                let service = ServiceInfo::new(
-                    "_beam._tcp.local.",
-                    "Beam",
-                    &host_name,
-                    current_ip,   
-                    53317,
-                    &properties[..],
-                )?;
-
-                let _ = daemon.register(service)?;
-
-                loop {
-                    std::thread::park();
-                }
+                register_service(&host_name, &current_ip, &code)?;    
+                
+                let listener = TcpListener::bind(&addr)?;
+                println!("TCP Listning addr: {}",&addr);
+                for stream in listener.incoming(){
+                    match stream {
+                        Ok(stream) => {
+                            println!("There are incoming");
+                            std::thread::spawn(|| handle_clinet(stream) );
+                        }
+                        Err(e) => {
+                            eprintln!("Error {}",e)
+                        }
+                    }
+        }
             }
             Err(e) => eprintln!("Error, {}", e),
         }
@@ -56,7 +85,6 @@ pub fn send(path: &str) -> std::result::Result<String, Box<dyn std::error::Error
     println!("{:?}",path);
     Ok(code)
 }
-
 
 
 pub fn recive(code:&str) ->std::result::Result<(), Box<dyn std::error::Error>> {
